64c67c8699b072580c84092265afa7bc6fca2393
Ismagod27 <106704712+Ismagod27@users.noreply.github.com>
2024-10-09 15:57:04 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,19 @@
+void main() {
+  // Variables y tipos de datos
+  String nombre = 'Ismanol';
+  int edad = 22;
+  String matricula = '2020-0098';
 
+  // Uso de un Map para almacenar información
+  Map<String, dynamic> informacion = {
+    'Nombre': nombre,
+    'Edad': edad,
+    'Matrícula': matricula
+  };
+
+  // Impresión de la información
+  print('Información del estudiante:');
+  print('Nombre: ${informacion['Nombre']}');
+  print('Edad: ${informacion['Edad']}');
+  print('Matrícula: ${informacion['Matrícula']}');
+}
